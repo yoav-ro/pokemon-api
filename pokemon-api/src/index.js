5dfcf95388386cc0d11628b21eb2f24912804afb
17Amir17 <amirangel101@hotmail.com>
2021-10-21 20:35:15 +0300

    get pokemon by name and id

--- pokemon-api/src/index.js
+++ pokemon-api/src/index.js
@@ -2,8 +2,14 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const pokemonRouter = require('./routers/pokemonRouter')
+const server = express();
+const port = 8080;
 
-console.log('Hello World!');
-console.log(path.basename('C:\\temp\\myfile.html'));
-const e = express();
-const c = cors();
+server.use("/pokemon", pokemonRouter); //Pokemon router
+
+// start the server
+server.listen(port, function() {
+  console.log(`Server Listening on port ${port}`);
+});
+
---
+++ pokemon-api/src/routers/pokemonRouter.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const Pokedex = require('pokedex-promise-v2');
+
+const router = express.Router();
+const P = new Pokedex();
+
+router.get('/get/:id', async (request, response) => {
+  try {
+    const pokemonID = request.params.id;
+    const pokemonData = await getPokemonData(pokemonID);
+    response.json(pokemonData);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get('/query', async (request, response) => {
+  try {
+    const pokemonName = request.query.query;
+    const pokemonData = await getPokemonData(pokemonName);
+    response.json(pokemonData);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+async function getPokemonData(query) {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const pokemonRawData = await P.getPokemonByName(query);
+      const pokemonData = {
+        name: pokemonRawData.name,
+        height: pokemonRawData.height,
+        weight: pokemonRawData.weight,
+        types: getTypes(pokemonRawData.types),
+        front_pic: pokemonRawData.sprites['front_default'],
+        back_pic: pokemonRawData.sprites['back_default'],
+        abilities: getAbilities(pokemonRawData.abilities),
+      };
+      resolve(pokemonData);
+    } catch (error) {
+      reject(error);
+    }
+  });
+}
+
+function getTypes(types) {
+  const newTypes = [];
+  types.forEach((type) => {
+    newTypes.push(type.type.name);
+  });
+  return newTypes;
+}
+
+function getAbilities(abilities){
+    const newAbilities = [];
+    abilities.forEach(abil => {
+        newAbilities.push(abil.ability.name);
+    })
+    return newAbilities;
+}
+
+module.exports = router;
