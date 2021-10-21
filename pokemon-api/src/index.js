2e096bd7cab8315e24c305842b182c78533a7511
17Amir17 <amirangel101@hotmail.com>
2021-10-21 18:12:07 +0300

    Skeleton

--- pokemon-api/src/index.js
+++ pokemon-api/src/index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const cors = require('cors');
 
 console.log('Hello World!');
 console.log(path.basename('C:\\temp\\myfile.html'));
 const e = express();
+const c = cors();
