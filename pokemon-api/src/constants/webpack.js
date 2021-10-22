e7a5b821b5d97266b5798738c435d1657189bcaa
17Amir17 <amirangel101@hotmail.com>
2021-10-22 20:38:20 +0300

    ignore

--- pokemon-api/src/constants/webpack.js
+++ pokemon-api/src/constants/webpack.js
@@ -1,3 +1,3 @@
-const webpack = false; //If true all files will be relative to dist folder if not relative to src
+const webpack = true; //If true all files will be relative to dist folder if not relative to src
 
 module.exports = webpack;
